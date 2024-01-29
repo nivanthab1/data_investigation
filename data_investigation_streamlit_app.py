219950c30e813cce7c81b0f9d56295b3e5c2f77e
NivAnalytics <148730209+nivanthab1@users.noreply.github.com>
2024-01-29 23:10:29 +1100

    Update data_investigation_streamlit_app.py

--- data_investigation_streamlit_app.py
+++ data_investigation_streamlit_app.py
@@ -39,21 +39,33 @@ uploaded_file = st.sidebar.file_uploader("*Upload file here*")
 if uploaded_file is not None:
     file_path = uploaded_file
 
-    #Caching file load data function
+    if ft == 'Excel':
+        try:
+            #User prompt to select sheet name in uploaded Excel
+            sh = st.sidebar.selectbox("*Which sheet name in the file should be read?*",pd.ExcelFile(file_path).sheet_names)
+            #User prompt to define row with column names if they aren't in the header row in the uploaded Excel
+            h = st.sidebar.number_input("*Which row contains the column names?*",0,100)
+        except:
+            st.info("File is not recognised as an Excel file")
+            sys.exit()
+    
+    elif ft == 'csv':
+        try:
+            #No need for sh and h for csv, set them to None
+            sh = None
+            h = None
+        except:
+            st.info("File is not recognised as a csv file.")
+            sys.exit()
+
+    #Caching function to load data
     @st.cache_data(experimental_allow_widgets=True)
-    def load_data(file_path,ft):
+    def load_data(file_path,ft,sh,h):
         
         if ft == 'Excel':
             try:
-                #User prompt to select sheet name in uploaded Excel
-                sh = st.sidebar.selectbox("*Which sheet name in the file should be read?*",pd.ExcelFile(file_path).sheet_names)
-
-                #User prompt to define row with column names if they aren't in the header row in the uploaded Excel
-                h = st.sidebar.number_input("*Which row contains the column names?*",0,100)
-
                 #Reading the excel file
                 data = pd.read_excel(file_path,header=h,sheet_name=sh,engine='openpyxl')
-
             except:
                 st.info("File is not recognised as an Excel file.")
                 sys.exit()
@@ -62,14 +74,13 @@ if uploaded_file is not None:
             try:
                 #Reading the csv file
                 data = pd.read_csv(file_path)
-
             except:
                 st.info("File is not recognised as a csv file.")
                 sys.exit()
         
         return data
 
-    data = load_data(file_path,ft)
+    data = load_data(file_path,ft,sh,h)
 #=======================================================================
 ## 0.2 Pre-processing datasets
 
